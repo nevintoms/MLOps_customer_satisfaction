23a78015455e288d6657cf2dd11ad5de867d6b1c
Nevin Toms <nevintoms333@gmail.com>
2025-07-16 15:13:12 -0400

    Add run pipeline

---
+++ run_pipeline.py
@@ -0,0 +1,6 @@
+from pipelines.training_pipeline import train_pipeline
+
+if __name__ == "__main__":
+    # Run the pipeline
+    train_pipeline(data_path=r"C:\Users\nevin\OneDrive\Desktop\Projects\MLOps\customer_satisfaction\data\olist_customers_dataset.csv")
+    

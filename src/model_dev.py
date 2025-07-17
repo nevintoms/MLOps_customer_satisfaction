f32fe1b7e66b78a86a9530f9c0677d409b3c9ada
Nevin Toms <nevintoms333@gmail.com>
2025-07-17 14:59:30 -0400

    update model development

---
+++ src/model_dev.py
@@ -0,0 +1,35 @@
+import logging
+from abc import ABC, abstractmethod
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+
+class Model(ABC):
+    """
+    Abstract base class for all models.
+    """
+
+    @abstractmethod
+    def train(self, X_train, y_train):
+        """
+        Trains the model on the given data.
+
+        Args:
+            x_train: Training data
+            y_train: Target data
+        """
+        pass
+
+class LinearRegressionModel(Model):
+    """
+    LinearRegressionModel that implements the Model interface.
+    """
+    def train(self, X_train, y_train, **kwargs):
+        try:
+            reg = LinearRegression(**kwargs)
+            reg.fit(X_train, y_train)
+            logging.info("Model Training completed")
+            return reg
+        except Exception as e:
+            logging.error("Error in training model: {}".format(e))
+            raise e

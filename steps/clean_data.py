86ce1cb190012e572a2f6c5df524a1d6772cdcce
Nevin Toms <nevintoms333@gmail.com>
2025-07-17 14:59:40 -0400

    update data clean

--- steps/clean_data.py
+++ steps/clean_data.py
@@ -1,7 +1,43 @@
 import logging
 import pandas as pd
 from zenml import step
+from typing import Tuple
+import pandas as pd
+from src.data_cleaning import (
+    DataCleaning,
+    DataDivideStrategy,
+    DataPreprocessStrategy,
+)
+from typing_extensions import Annotated
 
 @step
-def clean_df(df: pd.DataFrame) -> pd.DataFrame:
-    pass
+def clean_df(
+    df: pd.DataFrame,
+) -> Tuple[
+    Annotated[pd.DataFrame, "X_train"],
+    Annotated[pd.DataFrame, "X_test"],
+    Annotated[pd.Series, "y_train"],
+    Annotated[pd.Series, "y_test"],
+]:
+    """Data cleaning class which preprocesses the data and divides it into train and test data.
+
+    Args:
+        df: pd.DataFrame
+    Returns:
+        X_train: Training data
+        X_test: Testing data
+        y_train: Training labels
+        y_test: Testing labels
+    """
+    try:
+        preprocess_strategy = DataPreprocessStrategy()
+        data_cleaning = DataCleaning(df, preprocess_strategy)
+        preprocessed_data = data_cleaning.handle_data()
+
+        divide_strategy = DataDivideStrategy()
+        data_cleaning = DataCleaning(preprocessed_data, divide_strategy)
+        X_train, X_test, y_train, y_test = data_cleaning.handle_data()
+        return X_train, X_test, y_train, y_test
+    except Exception as e:
+        logging.error(e)
+        raise e

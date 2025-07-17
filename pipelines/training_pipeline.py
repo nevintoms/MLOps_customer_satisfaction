8f016398eeb0bdb4d91bee5fe69e4c026b06aa73
Nevin Toms <nevintoms333@gmail.com>
2025-07-17 14:58:47 -0400

    update training pipeline.

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -3,10 +3,11 @@ from steps.ingest_data import ingest_df
 from steps.clean_data import clean_df
 from steps.model_train import train_model
 from steps.evaluation import evaluate_model
+from steps.config import ModelNameConfig
 
 @pipeline(enable_cache=True)
 def train_pipeline(data_path: str):
     df = ingest_df(data_path)
-    clean_df(df)
-    train_model(df)
-    evaluate_model(df)
+    X_train, X_test, y_train, y_test = clean_df(df)
+    model = train_model(X_train, X_test, y_train, y_test, config=ModelNameConfig(model_name="LinearRegression"))
+    r2_score, rmse = evaluate_model(model, X_test, y_test)
